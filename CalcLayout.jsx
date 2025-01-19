1b7707c483e5fe053ae6cf5079fe7a4641e35c7d
AlmightyNeb <138020845+AlmightyNeb@users.noreply.github.com>
2025-01-19 01:20:04 -0500

    Create CalcLayout.jsx

---
+++ CalcLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CalcButton from "./CalcButton";
+
+const CalcLayout = (props) => {
+  return (
+    <>
+      <CalcButton num="1" />
+      <CalcButton num="2" />
+      <CalcButton num="3" />
+      <CalcButton num="4" />
+      <CalcButton num="5" />
+      <CalcButton num="6" />
+      <CalcButton num="7" />
+      <CalcButton num="8" />
+      <CalcButton num="9" />
+      <CalcButton num="0" />
+    </>
+  );
+};
